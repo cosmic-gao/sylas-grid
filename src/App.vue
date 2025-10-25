d588a367a92ed6cb110313f3c6827ee5b4cdf6fc
cosmic gao <cosmic.gao@outlook.com>
2025-10-25 11:22:55 +0800

    清空grid项目

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,6 @@
 <script setup lang="ts">
-import { ref } from "vue";
-
-import { type GridItemOptions } from "./core/type"
-import GridStack from "./components/GridStack.vue"
-import GridItem from "./components/GridItem.vue"
-
-const items = ref<GridItemOptions[]>([])
 </script>
 
 <template>
-  <GridStack>
-    <GridItem v-for="item in items" :key="item.id" v-bind="item">
 
-    </GridItem>
-  </GridStack>
 </template>
